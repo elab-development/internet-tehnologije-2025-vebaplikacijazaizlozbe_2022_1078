3068fed6096aa7f29b4f18f93dfe13a06f93fe1a
Njavex <vanja.zivkovic2002@gmail.com>
2026-01-16 12:28:41 +0100

    Pocetna Frontend struktura React, Vite, Tailwind CSS Postavljeno razvojno okruzenje i struktura Definisana struktura boja Globalni stil za scrollbar i text select

---
+++ WebAppZaIzlozbe/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+    return (
+        <Router>
+            <div className="min-h-screen bg-black text-white flex items-center justify-center">
+                <h1 className="text-4xl font-bold">Galerija Izložbi - Frontend Skeleton</h1>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
