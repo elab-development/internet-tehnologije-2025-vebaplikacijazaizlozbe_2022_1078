3765d2e4d2431e513a812a2d5fdf41fda9b0b1b1
sdimitrijevic3012 <sdimitrijevic02@gmail.com>
2026-01-19 21:15:44 +0100

    Ispravljen admin panel, ubacene poruke o greskama, poboljsana responzivnost

--- WebAppZaIzlozbe/frontend/src/pages/Register.jsx
+++ WebAppZaIzlozbe/frontend/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FiUser, FiMail, FiLock, FiPhone, FiAlertCircle, FiCheck } from 'react-icons/fi';
 import { useAuth } from '../context/AuthContext';
